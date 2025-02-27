321c9c1115d4bb09eb1ccbc0997c650d522e7bcf
Exist95 <jyeol0210@naver.com>
2025-02-27 17:29:07 +0900

    레이아웃 가운데 정렬로 변경

--- src/app/components/shared/common-layout.tsx
+++ src/app/components/shared/common-layout.tsx
@@ -4,6 +4,7 @@ import React from 'react'
 import '@/app/globals.css'
 import { TanstackQueryProvider } from '@/configs/tanstack-query/provider';
 import ThemeCounterButton from './theme-counter-button';
+import Toast from '../toast/toast';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -29,9 +30,10 @@ export default function CommonLayout({ children, title }: CommonLayoutProps) {
 
   return (
     <html lang="ko">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-gray-800 dark:text-white`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased dark:bg-gray-800 dark:text-white flex flex-col justify-center items-center p-4`} >
         <h1 className="text-2xl font-bold mb-4">{title}</h1>
         <ThemeCounterButton />
+        {/* <Toast /> */}
         <TanstackQueryProvider>{children}</TanstackQueryProvider>
       </body>
     </html>
