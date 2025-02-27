bf8a7f27dc6018169dc6320476e4756faa53c13a
Exist95 <jyeol0210@naver.com>
2025-02-27 17:48:50 +0900

    포켓몬 리스트 페이지 css 수정

--- src/app/components/pokemon/index.tsx
+++ src/app/components/pokemon/index.tsx
@@ -18,30 +18,37 @@ const PokemonPage = () => {
   }
 
   return (
-    <div>
-      <h1>포켓몬 리스트</h1>
-      <div>
-        <label htmlFor='limit'>limit:</label>
-        <input className="p-2 border rounded-md bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600" id='limit' value={limit} min={1} onChange={handleLimitChange} />
+    <div className='flex flex-col w-full mt-4'>
+
+      <div className='flex justify-center items-center gap-5'>
+        <div className='flex gap-2 items-center justify-center'>
+          <label htmlFor='limit' className='text-xl'>limit:</label>
+          <input className="p-2 border rounded-md bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600" id='limit' value={limit} min={1} onChange={handleLimitChange} />
+        </div>
+
+        <div className='flex gap-2 items-center justify-center'>
+          <label htmlFor='offset' className='text-xl'>offset:</label>
+          <input className="p-2 border rounded-md bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600" id='offset' value={offset} min={1} onChange={handleOffsetChange} />
+        </div>
       </div>
 
-      <div>
-        <label htmlFor='offset'>offset:</label>
-        <input className="p-2 border rounded-md bg-white text-black dark:bg-gray-800 dark:text-white dark:border-gray-600" id='offset' value={offset} min={1} onChange={handleOffsetChange} />
-      </div>
 
-      {isLoading && <p>로딩 중...</p>}
       {isError && <p>오류 발생: {error instanceof Error ? error.message : 'Unknown error'}</p>}
 
       <div className='flex justify-center items-center'>
-        <div className='grid grid-cols-4'>
-          {data?.map((pokemon: any) => (
-            <div key={pokemon.name}>
-              <Image src={pokemon.image} alt='image' width={600} height={800} />
-              <p>{pokemon.name}</p>
-            </div>
-          ))}
-        </div>
+        {isLoading
+          ?
+          <p className='flex justify-center items-center mt-6 text-xl'>포켓몬 리스트 로딩 중...</p>
+          :
+          <div className='grid grid-cols-4'>
+            {data?.map((pokemon: any) => (
+              <div key={pokemon.name}>
+                <Image src={pokemon.image} alt='image' width={600} height={800} />
+                <p>{pokemon.name}</p>
+              </div>
+            ))}
+          </div>
+        }
       </div>
     </div>
   )
