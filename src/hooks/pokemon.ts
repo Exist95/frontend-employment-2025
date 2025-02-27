1e011e1c9a67a32a8be6f7d9d7b29f9e070710b8
Exist95 <jyeol0210@naver.com>
2025-02-27 15:36:19 +0900

    포켓몬 훅 생성

---
+++ src/hooks/pokemon.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+
+const fetchPokemon = async (limit: number, offset: number) => {
+  const res = await fetch(`http://localhost:3000/api/pokemon?limit=${limit}&offset=${offset}`);
+  if (!res.ok) {
+    throw new Error('포켓몬 데이터 가져오기 실패')
+  }
+  return res.json();
+}
+
+export const usePokemonData = (limit: number, offset: number) => {
+  return useQuery({
+    queryKey: ['pokemon', limit, offset],
+    queryFn: () => fetchPokemon(limit, offset),
+  })
+}
