d19f49f6be3b53c8fb5b20496d17c0163266be15
Exist95 <jyeol0210@naver.com>
2025-03-05 17:38:54 +0900

    회원가입 로직 추가

--- src/app/(group2)/sign-up/client.tsx
+++ src/app/(group2)/sign-up/client.tsx
@@ -22,13 +22,28 @@ const SignUpClient = () => {
     setPassword2(e.target.value)
   }
 
+  const handleSignUp = () => {
+    if (!id) {
+      showToast('아이디를 입력해주세요', 'error')
+    } else if (!password) {
+      showToast('비밀번호를 입력해주세요', 'error')
+    } else if (!password2) {
+      showToast('비밀번호 확인을 입력해주세요', 'error')
+    } else if (password !== password2) {
+      showToast('비밀번호와 비밀번호 확인이 다릅니다', 'error')
+    } else {
+      //회원가입
+      //로컬스토리지에 저장
+    }
+  }
+
   return (
     <div className='flex flex-col justify-center items-center w-full my-4'>
       <div className='flex flex-col w-80 justify-center items-center gap-5'>
         <Input id='id' label='아이디' type='text' value={id} onChange={handleIdChange} />
         <Input id='password' label='비밀번호' type='password' value={password} onChange={handlePasswordChange} />
         <Input id='password2' label='비밀번호 확인' type='password' value={password2} onChange={handlePassword2Change} />
-        <Button size='full' value='회원가입' onClick={() => { }}
+        <Button size='full' value='회원가입' onClick={handleSignUp}
         />
       </div>
     </div>
